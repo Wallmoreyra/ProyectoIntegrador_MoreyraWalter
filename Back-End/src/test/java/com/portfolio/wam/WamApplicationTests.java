9b1b7bc37a9e70897a9f2f5133a85668d7e6cca4
Walter <49510922+Wallmoreyra@users.noreply.github.com>
2023-06-07 18:52:04 -0300

    Comit pru
    aaaaa

---
+++ Back-End/src/test/java/com/portfolio/wam/WamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.wam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
